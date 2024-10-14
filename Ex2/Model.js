7c16e7c671474bbc0fc746fe95e4f4e1b6d34a9a
C4d5 <163066432+C4d5@users.noreply.github.com>
2024-10-13 21:29:22 -0300

    Update Model.js

--- Ex2/Model.js
+++ Ex2/Model.js
@@ -1,51 +1,26 @@
-class Task {
-    constructor(description) {
-        this.description = description;
-        this.completed = false;
-    }
-}
-
 class TaskModel {
     constructor() {
         this.tasks = [];
     }
 
-    addTask(description) {
-        const newTask = new Task(description);
+    addTask(task) {
+        const newTask = { id: Date.now(), description: task, completed: false };
         this.tasks.push(newTask);
+        return newTask;
     }
 
-    removeTask(index) {
-        if (index >= 0 && index < this.tasks.length) {
-            this.tasks.splice(index, 1);
-        }
+    removeTask(taskId) {
+        this.tasks = this.tasks.filter(task => task.id !== taskId);
     }
 
-    toggleTask(index) {
-        if (index >= 0 && index < this.tasks.length) {
-            this.tasks[index].completed = !this.tasks[index].completed;
+    toggleTask(taskId) {
+        const task = this.tasks.find(task => task.id === taskId);
+        if (task) {
+            task.completed = !task.completed;
         }
     }
 
     getTasks() {
         return this.tasks;
-    }
-}
-
-export default TaskModel;
-
-            tasks.remove(index);
-        }
-    }
-
-    public void toggleTask(int index) {
-        if (index >= 0 && index < tasks.size()) {
-            Task task = tasks.get(index);
-            task.setCompleted(!task.isCompleted());
-        }
-    }
-
-    public List<Task> getTasks() {
-        return tasks;
     }
 }
